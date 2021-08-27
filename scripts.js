d3498a84472832f365b463fb163b48c8fedd0d12
Abdullayev99 <umarali1999email.ru@gmail.com>
2021-08-27 11:19:28 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// git clone link from git hub to get half ready projects
+// git pull
+// git status
+// git add .
+// git add -A
+// git config --global user.name "Umarali"
+// git config --global user.name "gmail.com"
+// git commit -m "first commit"
+// git push
+
+// LETS START OVER
+// 1 GIT INIT
+// 2 git config --local user.name "Ali"
+// 3 git config --local user.email hey@gmail.com
+// 4 git status
+// 5 git add -A OR .
